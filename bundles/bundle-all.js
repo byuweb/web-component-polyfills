fb5c9486a6bbba5056e60b879d554bc954a01398
Joseph Moore <yo.yo.yossarian@gmail.com>
2017-03-28 12:42:30 -0600

    replace skatejs polyfills with custom build of the webcomponentsjs polyfills

---
+++ bundles/bundle-all.js
@@ -0,0 +1,36 @@
+/*
+ *  @license
+ *    Copyright 2017 Brigham Young University
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *        http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+"use strict";
+
+//------ webcomponents-platform ------
+import '../bower_components/webcomponents-platform/webcomponents-platform.js'
+//------ template ------
+import '../bower_components/template/template.js'
+//------ es6-promise ------
+import '../bower_components/es6-promise/dist/es6-promise.auto.min.js'
+//------ pre-polyfill ------
+import '../bower_components/webcomponentsjs/src/pre-polyfill.js'
+//------ shadydom ------
+import '../bower_components/shadydom/src/shadydom.js'
+//------ custom-elements ------
+import '../bower_components/custom-elements/src/custom-elements.js'
+//------ shadycss ------
+import '../bower_components/shadycss/entrypoints/scoping-shim.js'
+//------ post-polyfill ------
+// import '../bower_components/webcomponentsjs/src/post-polyfill.js'
+//------ unresolved styles ------
+import '../bower_components/webcomponentsjs/src/unresolved.js'
